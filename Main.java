ef348555eb48a207682cb2a43f53bcba2010e3d4
Alyssa Weiglein <allie@alliew.dev>
2022-09-06 09:59:53 -0700

    initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+/*
+* Alyssa Weiglein
+* Final Coding Lab (RevUp)
+*/
+
+// class
+public class Main {
+  // method
+  public static void main(String[] args) {
+    Menu.menu(0, 0); // run main menu
+  }
+}
